fc9c156f170a4948868725b0d0b8b89f1c858935
sam <scpassaglia@gmail.com>
2022-02-09 22:22:13 +0900

    minor

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,25 @@
-#!/bin/bash
-directory=build
-branch=gh-pages
-build_command() {
-  python3 freeze.py
-}
-
-
-echo -e "\033[0;32mDeleting existing $branch...\033[0m"
-git push origin --delete $branch
-git branch -D $branch
-
-echo -e "\033[0;32mSetting up new $branch branch\033[0m"
-git checkout --orphan $branch
-git reset --hard
-git commit --allow-empty -m "Init"
+# just to get us all at the same starting point
 git checkout main
 
-echo -e "\033[0;32mDeleting old content...\033[0m"
-rm -rf $directory
+# delete your build folder
+rm -rf build/
 
-echo -e "\033[0;32mChecking out $branch....\033[0m"
-git worktree add $directory $branch
+# create a 'build' directory checked out to the gh-pages branch
+git worktree add -B gh-pages build origin/gh-pages
 
-echo -e "\033[0;32mGenerating site...\033[0m"
-build_command
+# build the project
+python3 freeze.py
 
-echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-cd $directory &&
-  git add --all &&
-  git commit -m "Deploy updates" &&
-  git push origin $branch
+# cd into 'build' folder, which is now on the gh-pages branch
+cd build
 
-echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree remove $directory
-git worktree prune
+# fail if for some reason this isn't the gh-pages branch
+current_branch=$(git symbolic-ref --short -q HEAD)
+if [ "$current_branch" != "gh-pages" ]; then
+  echo "Expected build folder to be on gh-pages branch."
+  exit 1
+fi
+
+# commit and push to gh-pages
+git add . && git commit -m "Update gh-pages"
+git push
