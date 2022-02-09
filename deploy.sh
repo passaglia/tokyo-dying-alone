0b94638374e50f64f9c4d2ec09382782df3a1f63
sam <scpassaglia@gmail.com>
2022-02-09 22:50:07 +0900

    minor; "

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,5 @@
-# just to get us all at the same starting point
-git checkout main
+python3 freeze.py 
 
-# delete your build folder
-rm -rf build/
+git push origin :gh-pages
 
-# create a 'build' directory checked out to the gh-pages branch
-git worktree add -B gh-pages build origin/gh-pages
-
-# build the project
-python3 freeze.py
-
-# cd into 'build' folder, which is now on the gh-pages branch
-cd build
-
-# fail if for some reason this isn't the gh-pages branch
-current_branch=$(git symbolic-ref --short -q HEAD)
-if [ "$current_branch" != "gh-pages" ]; then
-  echo "Expected build folder to be on gh-pages branch."
-  exit 1
-fi
-
-# commit and push to gh-pages
-git add . && git commit -m "Update gh-pages"
-git push
+git subtree push --prefix build origin gh-pages
