a3b3b1ca70b1385a2f45398bfbc20b2b81c88695
sam <scpassaglia@gmail.com>
2022-02-20 14:29:38 +0900

    minor

--- static/lib/dc.leaflet/index.js
+++
@@ -1,7 +0,0 @@
-d3 = require("d3");
-crossfilter = require("crossfilter2");
-dc = require("dc");
-leaflet = require('leaflet');
-leaflet.markercluster = require('leaflet.markercluster');
-
-module.exports = require("./dc.leaflet");
